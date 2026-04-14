55d687ddf0c345dadfb9ae874a14227337ef9bd0
Christina Hanson <christinahansondesu@gmail.com>
2026-04-14 16:10:30 +1200

    The mod.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Homepage Background
+A Town of Salem 2 mod to replace the background on your home screen with a custom image. Can also be used to hide the news panel.
+
+## Installing
+This mod requires [Salem Mod Loader](https://github.com/Curtbot9000/SalemModLoader), but is not available on its mod browser yet.
+After installing SML, you can [download the latest release of Homepage Background](https://github.com/LyricLy/HomepageBackground/releases/latest) and copy the .dll file to the `Town of Salem 2/SalemModLoader/Mods/` directory.
+
+## Configuring
+Place your background image in `Town of Salem 2/SalemModLoader/ModFolders/HomepageBackground/background.png`. In-game configuration in the Salem Mod Loader menu can be used to enable hiding the news panel.
---
+++ src/HomepageBackground.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using SML;
+using HarmonyLib;
+using Home.HomeScene;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace HomepageBackground;
+
+[Mod.SalemMod]
+public class HomepageBackground
+{
+    static Texture2D backgroundTexture = null;
+
+    public static void Start()
+    {
+        Harmony.CreateAndPatchAll(typeof(HomepageBackground));
+
+        string dir = Path.GetDirectoryName(UnityEngine.Application.dataPath) + "/SalemModLoader/ModFolders/HomepageBackground/";
+        byte[] data;
+
+        try
+        {
+            data = File.ReadAllBytes(dir + "background.png");
+        }
+        catch (DirectoryNotFoundException)
+        {
+            Directory.CreateDirectory(dir);
+            return;
+        }
+        catch (FileNotFoundException)
+        {
+            return;
+        }
+
+        backgroundTexture = new Texture2D(2, 2);
+        ImageConversion.LoadImage(backgroundTexture, data);
+    }
+
+    static void ClearNews(HomeSceneController controller)
+    {
+        if (ModSettings.GetBool("Hide news tab", "lyricly.homepagebackground"))
+        {
+            controller.homeNewsTabElementsUI.SetActive(false);
+        }
+    }
+
+    static void AddBackground(HomeSceneController controller)
+    {
+        if (!backgroundTexture)
+        {
+            return;
+        }
+
+        var background = new GameObject("HomepageBackground");
+        background.transform.SetParent(controller.HudCanvas.transform);
+        background.transform.localPosition = Vector3.zero;
+        background.transform.localScale = Vector3.one;
+        background.transform.SetAsFirstSibling();
+
+        var im = background.AddComponent<RawImage>();
+        im.texture = backgroundTexture;
+        im.rectTransform.anchorMin = Vector2.zero;
+        im.rectTransform.anchorMax = Vector2.one;
+        im.rectTransform.sizeDelta = Vector2.zero;
+    }
+
+    [HarmonyPatch(typeof(HomeSceneController), "Start")]
+    [HarmonyPostfix]
+    public static IEnumerator ShowBackground(IEnumerator original, HomeSceneController __instance)
+    {
+        AddBackground(__instance);
+
+        while (original.MoveNext())
+        {
+            yield return original.Current;
+        }
+
+        ClearNews(__instance);
+    }
+
+    [HarmonyPatch(typeof(HomeSceneController), "CloseGameModeSelectionPopup")]
+    [HarmonyPostfix]
+    public static void KeepNewsHidden(HomeSceneController __instance)
+    {
+        ClearNews(__instance);
+    }
+}
